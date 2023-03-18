2c49d836e78c7791554f0fceaacaa00329f8af38
JaeranKIM <devjaeran@gmail.com>
2023-03-19 00:08:28 +0900

    FEAT: update에서 기본값으로 보여주는 input값 관련 해결중

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { __addPost } from "../redux/modules/PostSlice";
 import Button from "./../components/Button";
 import Input from "./../components/Input";
